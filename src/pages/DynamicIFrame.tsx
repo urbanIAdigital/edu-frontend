1a53cd8cc440a6d2b216c4631027a8188f87d23e
Juan Carlos Carrasquilla <juan.carrasquilla@edu.gov.co>
2024-09-10 15:30:57 -0500

    add new menu and default img

--- src/pages/DynamicIFrame.tsx
+++ src/pages/DynamicIFrame.tsx
@@ -1,17 +1,31 @@
 import { useParams } from "react-router-dom";
 import { contratos } from "../constants/data_resp.json";
+import { Box } from "@mui/material";
+import defaultBg from "../assets/Fondo_V3.png";
+
 const DynamicIFrame = () => {
   const { contrato_id } = useParams();
   const contrato = contratos.find(
     (contrato) => contrato.redirect === contrato_id
   );
   return (
-    <iframe
-      src={contrato?.url}
-      height="100%"
-      width="100%"
-      sandbox="allow-scripts allow-same-origin"
-    ></iframe>
+    <>
+      {contrato?.url ? (
+        <iframe
+          src={contrato?.url}
+          height="100%"
+          width="100%"
+          sandbox="allow-scripts allow-same-origin"
+        ></iframe>
+      ) : (
+        <Box
+          component={"img"}
+          src={defaultBg}
+          sx={{ objectFit: "contain" }}
+          height={"100%"}
+        />
+      )}
+    </>
   );
 };
 
