4fbec3d79fd4fe689d85b12619a6828c9006d919
Juan Carlos Carrasquilla <juan.carrasquilla@edu.gov.co>
2024-09-10 10:21:24 -0500

    fix(deploy)

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'edu-frontend', // Set the base path for GitHub Pages deployment
+  base: 'edu-frontend',
 })
